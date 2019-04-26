e788e44b3b69e31e2db51996e639af64439997b3
Lambda_School_Loaner_98 <henry.neal0@gmail.com>
2019-04-26 12:24:27 -0400

    mvp done

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,7 +1,13 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
 
-
+class City:
+  def __init__(self, name, latitude, longitude):
+    self.name = name
+    self.latitude = latitude
+    self.longitude = longitude
+  def __str__(self):
+      return f"{self.name}: ({self.latitude}, {self.longitude})"
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -14,13 +20,19 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
-cities = []
 
-def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
+# TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+
+cities = []
+import csv
+def cityreader(cities=[]):
+  with open('cities.csv') as csvfile:
+    spamreader = csv.reader(csvfile)
+    for row in spamreader:
+      print((', \n').join(row))
+
     return cities
 
 cityreader(cities)
